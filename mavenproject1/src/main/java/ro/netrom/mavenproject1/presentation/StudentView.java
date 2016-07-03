c78f6693eb69fdf62ced8c2563e8596faaae8a95
ica-alexandra <alexandra_claudiaica@yahoo.com>
2016-07-03 16:32:34 +0300

    03.07

--- mavenproject1/src/main/java/ro/netrom/mavenproject1/presentation/StudentView.java
+++ mavenproject1/src/main/java/ro/netrom/mavenproject1/presentation/StudentView.java
@@ -6,9 +6,12 @@
 package ro.netrom.mavenproject1.presentation;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import ro.netrom.mavenproject1.business.entity.Event;
 import ro.netrom.mavenproject1.business.entity.Student;
 
 /**
@@ -33,5 +36,32 @@ public class StudentView implements Serializable{
     public void init(){
         student = new Student();
         student.setFirstName("Alexandra");
+        student.setLastName("Ica");
+        student.setGender(Student.Gender.FEMALE);
+        /*Date birthDate = new Date();
+        birthDate.setMonth(5);
+        birthDate.setYear(1995);
+        student.setBirthDate(birthDate);*/
+        student.setEmail("alexandra_claudiaica@yahoo.com");
+        student.setPhoneNumbers("0748566892");
+        student.setFaculty("Autoamtica, Calculatoare si Electronica");
+        student.setFacultyStartYear(2014);
+        /*Event event = new Event();
+        event.setDepartment(Event.Department.JAVA);
+        event.setEventType(Event.EventType.PRACTICE);
+        Date date = new Date();
+        date.setDate(27);
+        date.setMonth(6);
+        date.setYear(2016);
+        event.setStartDate(date);
+        date.setDate(17);
+        date.setMonth(7);
+        date.setYear(2016);
+        event.setEndDate(date);
+        event.setTask("Make an application");
+        date.setDate(27);
+        date.setMonth(6);
+        date.setYear(2016);
+        student.setEvents((List<Event>) event);*/
     }
 }
