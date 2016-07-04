d379e23884c911d0be01c62a0625680ee638b106
valentindg <valentin.f.dogaru@gmail.com>
2016-07-04 16:24:57 +0300

    project 04.07.2016

--- practica01/src/main/java/ro/netrom/practica01/bussnies/boundary/Students.java
+++ practica01/src/main/java/ro/netrom/practica01/bussnies/boundary/Students.java
@@ -5,14 +5,41 @@
  */
 package ro.netrom.practica01.bussnies.boundary;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import ro.netrom.practica01.bussnies.entity.Student;
 
 /**
  *
  * @author practice11
  */
-public class Students {
+@Stateless
+public class Students implements Serializable {
+
     @PersistenceContext(name = "practicapu")
     private EntityManager em;
+
+    public void studentSave(Student student) {
+        em.persist(student);
+    }
+
+    public void studentEdit(Student student) {
+        em.persist(student);
+    }
+
+    public void studentDelete(Student student) {
+        em.persist(student);
+    }
+
+    public List<Student> getAllStudents() {
+        final String query = "SELECT s from Student s";       
+    TypedQuery<Student> query1 = em.createQuery(query, Student.class);
+    final List<Student> students = query1.getResultList();
+    return students;
+    }
+
 }
